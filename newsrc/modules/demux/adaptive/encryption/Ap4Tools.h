86fa8a1ee819745cd5023c0a9b1f529c863ac974
kobe84762 <320700738+kobe84762@users.noreply.github.com>
2026-08-29 23:28:20 +0000

    Create Ap4Tools.h

---
+++ newsrc/modules/demux/adaptive/encryption/Ap4Tools.h
@@ -0,0 +1,32 @@
+#ifndef _AP4_TOOLS_H_
+#define _AP4_TOOLS_H_
+
+#include <vlc_block.h>
+#include <string>
+
+class Ap4Tools final {
+private:
+	friend class AP4_CencTrackEncryption;
+	Ap4Tools() = delete;
+	Ap4Tools(const Ap4Tools& other) = delete;
+	Ap4Tools& operator=(const Ap4Tools& other) = delete;
+	Ap4Tools(Ap4Tools&& other) = delete;
+	Ap4Tools& operator=(Ap4Tools&& other) = delete;
+
+	void* operator new(size_t);
+	void* operator new(size_t, void*);
+	void  operator delete(void*);
+	void* operator new[](size_t);
+	void* operator new[](size_t, void*);
+	void  operator delete[](void*);
+
+public:
+	static std::string getKeyId(block_t **initData);
+	static void decrypt(block_t **segment);
+	static bool removeAtom(block_t **segment, const char *atom);
+
+private:
+	inline static std::string keyId = std::string("");
+};
+
+#endif // !_AP4_TOOLS_H_
