e1b246d74d5f1c2a444ccbb8a1d5fefd83094b0e
kobe84762 <320700738+kobe84762@users.noreply.github.com>
2026-08-30 13:47:54 +0000

    Update CommonEncryption.cpp

--- newsrc/modules/demux/adaptive/encryption/CommonEncryption.cpp
+++ newsrc/modules/demux/adaptive/encryption/CommonEncryption.cpp
@@ -97,6 +97,28 @@ bool CommonEncryptionSession::start(SharedResources *res, const CommonEncryption
         }
         ctx = handle;
     }
+    else if(encryption.method == CommonEncryption::Method::AES_128_CTR)
+    {
+        if(key.empty())
+        {
+            if(!encryption.uri.empty())
+                key = res->getKeyring()->getKey(res, encryption.uri);
+            if(key.size() != 16)
+                return false;
+        }
+
+        vlc_gcrypt_init();
+        gcry_cipher_hd_t handle;
+        if( gcry_cipher_open(&handle, GCRY_CIPHER_AES, GCRY_CIPHER_MODE_CTR, 0) ||
+                gcry_cipher_setkey(handle, &key[0], 16) ||
+                gcry_cipher_setiv(handle, &encryption.iv[0], 16) )
+        {
+            gcry_cipher_close(handle);
+            ctx = nullptr;
+            return false;
+        }
+        ctx = handle;
+    }
 #endif
     return true;
 }
