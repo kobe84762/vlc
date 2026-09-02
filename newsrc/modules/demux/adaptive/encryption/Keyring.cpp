c8489de403cfd8a13f35047abbcbe51c242c97cf
kobe84762 <320700738+kobe84762@users.noreply.github.com>
2026-09-02 20:57:12 +0000

    Update Keyring.cpp

--- newsrc/modules/demux/adaptive/encryption/Keyring.cpp
+++ newsrc/modules/demux/adaptive/encryption/Keyring.cpp
@@ -98,51 +98,6 @@ KeyringKey Keyring::getKey(SharedResources *resources, const std::string &uri)
     return key;
 }
 
-std::string Keyring::getClearKey(const std::string &uri)
-{
-    std::string keyString;
-    KeyringKey key;
-
-    vlc_mutex_locker locker(&lock);
-    std::map<std::string, KeyringKey>::iterator it = keys.find(uri);
-    if(it == keys.end())
-    {
-        /* Pretty bad inside the lock */
-        msg_Dbg(obj, "Retrieving AES key %s", uri.c_str());
-        block_t *p_block = Retrieve::HTTP(resources, http::ChunkType::Key, uri);
-        if(p_block)
-        {
-            if(p_block->i_buffer == 16)
-            {
-                key.resize(16);
-                memcpy(&key[0], p_block->p_buffer, 16);
-                keyString = rawKeyToHex(key);
-                keys.insert(std::pair<std::string, std::string>(uri, key));
-                lru.push_front(uri);
-                if(lru.size() > Keyring::MAX_KEYS)
-                {
-                    keys.erase(keys.find(lru.back()));
-                    lru.pop_back();
-                }
-            }
-            block_Release(p_block);
-        }
-    }
-    else
-    {
-        std::list<std::string>::iterator it2 = std::find(lru.begin(), lru.end(), uri);
-        if(it2 != lru.begin())
-        {
-            lru.erase(it2);
-            lru.push_front(uri);
-        }
-        key = (*it).second;
-        keyString = rawKeyToHex(key);
-    }
-
-    return keyString;
-}
-
 std::string Keyring::getCustomKey(const std::vector<unsigned char> &keyId) const
 {
     const std::string kid(keyId.begin(), keyId.end());
