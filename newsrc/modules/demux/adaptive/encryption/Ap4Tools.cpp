7f551905ce0dd9f27320688ea29edf36877855e8
kobe84762 <320700738+kobe84762@users.noreply.github.com>
2026-09-07 21:42:12 +0000

    Update Ap4Tools.cpp

--- newsrc/modules/demux/adaptive/encryption/Ap4Tools.cpp
+++ newsrc/modules/demux/adaptive/encryption/Ap4Tools.cpp
@@ -1,16 +1,14 @@
-#include <vlc_block.h>
-
 #include "Ap4.h"
 #include "Ap4Tools.h"
 
-std::string Ap4Tools::getKeyId(block_t **initData) {
+std::string Ap4Tools::getKeyId(void *inputdata, size_t inputbytes) {
 
-    if ((*initData)->i_buffer < 1)
+    if (inputbytes < 1)
         return { };
 
     keyId.clear();
 
-    AP4_MemoryByteStream* stream = new AP4_MemoryByteStream((*initData)->p_buffer, (*initData)->i_buffer);
+    AP4_MemoryByteStream* stream = new AP4_MemoryByteStream(reinterpret_cast<unsigned char *>(inputdata), inputbytes);
     AP4_File file = AP4_File(*stream, true);
 
     stream->Release();
